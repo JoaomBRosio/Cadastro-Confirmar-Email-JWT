d00ca0352a8842b31224e093477c7c45d5f685ed
João Goldoni Ambrósio <125475444+JoaomBRosio@users.noreply.github.com>
2025-01-30 15:41:39 -0300

    Update config.py

--- cadastro_confirmarEmail_jwt/config.py
+++ cadastro_confirmarEmail_jwt/config.py
@@ -7,4 +7,4 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'jgadgaambrosio@gmail.com'
-    MAIL_PASSWORD = 'qghu hciv bxbe ynkk'  # Substitua por uma senha real ou App Password
+    MAIL_PASSWORD = ''  # Substitua por uma senha real ou App Password
